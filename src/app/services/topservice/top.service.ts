f68c4ad8f60f0687677f7f47ccde35bf96c6e4dc
Adam Bennett <nyoxidetwitter@gmail.com>
2020-05-21 20:16:47 -0400

    fixed URL

--- src/app/services/topservice/top.service.ts
+++ src/app/services/topservice/top.service.ts
@@ -8,8 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class TopService {
 
-  // public API = 'https://sts-duelist-metrics.herokuapp.com/';
-  public API = 'http://localhost:8080/';
+  public API = 'https://sts-duelist-metrics.herokuapp.com/';
+  //public API = 'http://localhost:8080/';
 
   constructor(private http: HttpClient) { }
 
